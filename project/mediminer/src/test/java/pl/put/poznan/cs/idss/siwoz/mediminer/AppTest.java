462f6b1bcdb17067555e6b35daff3cc02264a948
Małgorzata Napieraj <malgorzata.napieraj@student.put.poznan.pl>
2014-04-16 23:46:48 +0200

    Zakonczenie prac nad importem oraz eksportem plikow.

--- project/mediminer/src/test/java/pl/put/poznan/cs/idss/siwoz/mediminer/AppTest.java
+++ project/mediminer/src/test/java/pl/put/poznan/cs/idss/siwoz/mediminer/AppTest.java
@@ -61,49 +61,49 @@ public class AppTest extends TestCase {
 	}
 
 	public void testFromInstancesConverter() {
-//		testFromInstancesConverter(new InstancesToCSVConverter(),
-//			new CSVToInstancesConverter(), ".csv");
-//		testFromInstancesConverter(new InstancesToArffConverter(),
-//				new ArffToInstancesConverter(), ".arff");
+		testFromInstancesConverter(new InstancesToCSVConverter(),
+			new CSVToInstancesConverter(), ".csv");
+		testFromInstancesConverter(new InstancesToArffConverter(),
+				new ArffToInstancesConverter(), ".arff");
 		testFromInstancesConverter(new InstancesToXlsConverter(),
 				new XlsToInstancesConverter(), ".xls");
 	}
 
 	public void testToInstancesConverter() {
-//		try {
-//			String path1 = System.getProperty("user.dir")
-//					+ "/src/test/resources/test.arff";
-//			String path2 = System.getProperty("user.dir")
-//					+ "/src/test/resources/test.csv";
-//			String path3 = System.getProperty("user.dir")
-//					+ "/src/test/resources/test1.xls";
-//			IToInstancesConverter converter1 = new ArffToInstancesConverter();
-//			Instances data1 = converter1.parseToInstances(path1);
-//			IToInstancesConverter converter2 = new CSVToInstancesConverter();
-//			Instances data2 = converter2.parseToInstances(path2);
-//			IToInstancesConverter converter3 = new XlsToInstancesConverter();
-//			Instances data3 = converter3.parseToInstances(path3);
-//			IBk c1 = new IBk(1);
-//			IBk c2 = new IBk(1);
-//			IBk c3 = new IBk(1);
-//			c1.buildClassifier(data1);
-//			c2.buildClassifier(data2);
-//			c3.buildClassifier(data3);
-//			for (int i = 0; i < data1.numInstances(); i++) {
-//				double result1 = c1.classifyInstance(data1.instance(i));
-//				double result2 = c2.classifyInstance(data2.instance(i));
-//				double result3 = c3.classifyInstance(data3.instance(i));
-//				String sAttr1 = data1.classAttribute().value((int) result1);
-//				String sAttr2 = data2.classAttribute().value((int) result2);
-//				String sAttr3 = data3.classAttribute().value((int) result3);
-//				// System.out.println(sAttr1 + " " + sAttr2+ " " + sAttr3);
-//				assertEquals(sAttr1, sAttr2);
-//				assertEquals(sAttr1, sAttr3);
-//				assertEquals(sAttr2, sAttr3);
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
+		try {
+			String path1 = System.getProperty("user.dir")
+					+ "/src/test/resources/test.arff";
+			String path2 = System.getProperty("user.dir")
+					+ "/src/test/resources/test.csv";
+			String path3 = System.getProperty("user.dir")
+					+ "/src/test/resources/test1.xls";
+			IToInstancesConverter converter1 = new ArffToInstancesConverter();
+			Instances data1 = converter1.parseToInstances(path1);
+			IToInstancesConverter converter2 = new CSVToInstancesConverter();
+			Instances data2 = converter2.parseToInstances(path2);
+			IToInstancesConverter converter3 = new XlsToInstancesConverter();
+			Instances data3 = converter3.parseToInstances(path3);
+			IBk c1 = new IBk(1);
+			IBk c2 = new IBk(1);
+			IBk c3 = new IBk(1);
+			c1.buildClassifier(data1);
+			c2.buildClassifier(data2);
+			c3.buildClassifier(data3);
+			for (int i = 0; i < data1.numInstances(); i++) {
+				double result1 = c1.classifyInstance(data1.instance(i));
+				double result2 = c2.classifyInstance(data2.instance(i));
+				double result3 = c3.classifyInstance(data3.instance(i));
+				String sAttr1 = data1.classAttribute().value((int) result1);
+				String sAttr2 = data2.classAttribute().value((int) result2);
+				String sAttr3 = data3.classAttribute().value((int) result3);
+				// System.out.println(sAttr1 + " " + sAttr2+ " " + sAttr3);
+				assertEquals(sAttr1, sAttr2);
+				assertEquals(sAttr1, sAttr3);
+				assertEquals(sAttr2, sAttr3);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
