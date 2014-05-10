71b402a125770948c3baf7f718aa672ea1a22b90
Kornel Lewandowski <kornel.lewandowski@student.put.poznan.pl>
2014-05-10 16:03:09 +0200

    Kornel - dodanie akcji w RestHandlerze

---
+++ project/mediminer/src/main/java/pl/put/poznan/cs/idss/siwoz/mediminer/handler/RestHandler.java
@@ -0,0 +1,279 @@
+package pl.put.poznan.cs.idss.siwoz.mediminer.handler;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import pl.put.poznan.cs.idss.siwoz.mediminer.converter.loader.ArffToInstancesConverter;
+import pl.put.poznan.cs.idss.siwoz.mediminer.converter.loader.CSVToInstancesConverter;
+import pl.put.poznan.cs.idss.siwoz.mediminer.converter.loader.IToInstancesConverter;
+import pl.put.poznan.cs.idss.siwoz.mediminer.converter.loader.XlsToInstancesConverter;
+import pl.put.poznan.cs.idss.siwoz.mediminer.converter.saver.IFromInstancesConverter;
+import pl.put.poznan.cs.idss.siwoz.mediminer.converter.saver.InstancesToArffConverter;
+import weka.core.Attribute;
+import weka.core.Instance;
+import weka.core.Instances;
+
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * Handler obsługujący żądania RESTowe z aplikacji. Zapisuje dane z przeglądarki
+ * do pliku tekstowego oraz odczytuje takowe dane.
+ * 
+ * @author Kornel Lewandowski
+ * 
+ * @date 4 sty 2014 10:56:55
+ * 
+ */
+public class RestHandler extends AbstractHandler {
+
+	private static Logger LOGGER = Logger.getLogger(RestHandler.class
+			.toString());
+
+	public static String KEY = Double
+			.toString(Math.round(Math.random() * 100000));
+
+	private static final MultipartConfigElement MULTI_PART_CONFIG = new MultipartConfigElement(
+			System.getProperty("user.dir"));
+
+	private String currentDataFileName = null;
+	private String currentModelFileName = null;
+	private Instances instancesContainer = null;
+
+	public void handle(String target, Request baseRequest,
+			HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
+		Map<String, String[]> params = request.getParameterMap();
+
+		if (request.getContentType() != null
+				&& request.getContentType().startsWith("multipart/form-data")) {
+			baseRequest.setAttribute(Request.__MULTIPART_CONFIG_ELEMENT,
+					MULTI_PART_CONFIG);
+			request.setAttribute(Request.__MULTIPART_CONFIG_ELEMENT,
+					MULTI_PART_CONFIG);
+
+		}
+
+		if (params.containsKey("action")) {
+
+			String action = params.get("action")[0];
+
+			if (action.equals("import-file")) {
+				importFile(request, response, params, action);
+			}
+
+			else if (action.equals("send-key")) {
+				sendKey(request, response, params);
+
+			} else if (action.equals("get-attributes")) {
+
+				getAttributes(request, response);
+
+			} else if (action.equals("get-instances")) {
+
+				getInstances(request, response);
+
+			} else if (action.equals("export-arff")) {
+
+				exportArff(request, response);
+
+			} else {
+
+				returnError(request, response, params, "Action not supported!");
+
+			}
+
+		} else {
+			returnError(request, response, params, "Action not selected!");
+
+		}
+
+	}
+
+	private void getInstances(HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+		if (instancesContainer != null) {
+			response.setContentType("text/plain; charset=UTF-8");
+			List<Instance> instances = new ArrayList<>();
+			for (int i = 0; i < instancesContainer.numInstances(); i++) {
+				instances.add(instancesContainer.instance(i));
+			}
+			Gson gson = new GsonBuilder().serializeSpecialFloatingPointValues()
+					.addSerializationExclusionStrategy(new ExclusionStrategy() {
+
+						@Override
+						public boolean shouldSkipField(FieldAttributes field) {
+							return field.getName().equals("m_Dataset");
+						}
+
+						@Override
+						public boolean shouldSkipClass(Class<?> clazz) {
+							return clazz == Instances.class;
+						}
+					}).create();
+			String gsonResponse = gson.toJson(instances);
+			gsonResponse = gsonResponse.replace("Infinity", "99999999.999");
+			gsonResponse = gsonResponse.replace("NaN", "0.00");
+			response.getWriter().println(gsonResponse);
+			((Request) request).setHandled(true);
+		} else {
+			response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+			((Request) request).setHandled(true);
+		}
+	}
+
+	private void getAttributes(HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+		if (instancesContainer != null) {
+			response.setContentType("text/plain; charset=UTF-8");
+			List<Attribute> attributes = new ArrayList<>();
+			for (int i = 0; i < instancesContainer.numAttributes(); i++) {
+				attributes.add(instancesContainer.attribute(i));
+			}
+			Gson gson = new GsonBuilder().serializeSpecialFloatingPointValues()
+					.create();
+			String gsonResponse = gson.toJson(attributes);
+			gsonResponse = gsonResponse.replace("Infinity", "99999999.999");
+			gsonResponse = gsonResponse.replace("NaN", "0.00");
+			response.getWriter().println(gsonResponse);
+			((Request) request).setHandled(true);
+		} else {
+			response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+			((Request) request).setHandled(true);
+		}
+	}
+
+	private void returnError(HttpServletRequest request,
+			HttpServletResponse response, Map<String, String[]> params,
+			final String message) {
+
+		LOGGER.log(Level.SEVERE, "ERROR: " + message);
+
+		try {
+			response.setContentType("text/plain");
+			response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+			response.getWriter().println(message);
+		} catch (Exception e) {
+			response.setContentType("text/plain");
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		}
+
+		((Request) request).setHandled(true);
+
+	}
+
+	private void sendKey(HttpServletRequest request,
+			HttpServletResponse response, Map<String, String[]> params) {
+		try {
+			response.getWriter().println(KEY);
+		} catch (IOException e) {
+			response.setContentType("text/plain");
+			response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+			((Request) request).setHandled(true);
+		}
+
+		((Request) request).setHandled(true);
+	}
+
+	private void exportArff(HttpServletRequest request,
+			HttpServletResponse response) {
+
+		String filePath = "";
+
+		try {
+
+			if (instancesContainer == null) {
+				throw new Exception("There is not file loaded");
+			}
+
+			IFromInstancesConverter arffConverter = new InstancesToArffConverter();
+			arffConverter.parseFromInstances(instancesContainer, "MediMiner"
+					+ new Date() + ".arff");
+			response.getWriter().println(filePath);
+		} catch (Exception e) {
+			response.setContentType("text/plain");
+			response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+			((Request) request).setHandled(true);
+		}
+
+		((Request) request).setHandled(true);
+	}
+
+	// private void openFile(HttpServletRequest request,
+	// HttpServletResponse response, Map<String, String[]> params,
+	// String action) {
+	//
+	// }
+
+	private void importFile(HttpServletRequest request,
+			HttpServletResponse response, Map<String, String[]> params,
+			String action) throws IOException {
+
+		try {
+
+			for (Part part : request.getParts()) {
+
+				String FILE_NAME = "TMP_" + KEY + "_"
+						+ part.getSubmittedFileName();
+				part.write(FILE_NAME);
+
+				currentDataFileName = FILE_NAME;
+
+			}
+
+			if (currentDataFileName.endsWith("xls")
+					|| currentDataFileName.endsWith("XLS")) {
+				IToInstancesConverter xlsConverter = new XlsToInstancesConverter();
+				instancesContainer = xlsConverter
+						.parseToInstances(currentDataFileName);
+
+			} else if (currentDataFileName.endsWith("csv")
+					|| currentDataFileName.endsWith("CSV")) {
+				IToInstancesConverter csvConverter = new CSVToInstancesConverter();
+				instancesContainer = csvConverter
+						.parseToInstances(currentDataFileName);
+
+			} else if (currentDataFileName.endsWith("arff")
+					|| currentDataFileName.endsWith("ARFF")) {
+				IToInstancesConverter arffConverter = new ArffToInstancesConverter();
+				instancesContainer = arffConverter
+						.parseToInstances(currentDataFileName);
+
+			} else {
+				throw new Exception(
+						"This file extension is not supported. Try to import CSV, XLS or ARFF file.");
+			}
+
+			response.setContentType("text/plain");
+			response.setStatus(HttpServletResponse.SC_FOUND);
+			response.sendRedirect("../");
+
+		} catch (Exception e) {
+			response.setContentType("text/plain");
+			response.setStatus(HttpServletResponse.SC_NOT_ACCEPTABLE);
+			response.getWriter().println(e.getMessage());
+			response.sendRedirect("../?error=import-error");
+		}
+
+		((Request) request).setHandled(true);
+
+	}
+
+}
--- project/mediminer/src/main/resources/js/main.js
+++ project/mediminer/src/main/resources/js/main.js
@@ -1,62 +1,140 @@
 $(document).ready(function() {
 
-	function showError(message) {
-		$('div#error-box').show();
-		$('div#error-box').html(message);
-	}
+    console.log('Main view loaded');
 
-	function showSuccess(message) {
-		$('div#success-box').show();
-		$('div#success-box').html(message);
-	}
+    var actions = new Actions();
 
-	console.log('Main view loaded');
+    // menu buttons
+    $('li#import-data').click(function(e) {
+	e.preventDefault();
+	$('input#file_to_open').trigger('click');
+    });
 
-	// menu buttons
-	$('li#import-data').click(function(e) {
-		e.preventDefault();
-		$('input#file_to_open').trigger('click');
-	});
+    $('input#file_to_open').change(function() {
+	$('form#open_file_form').submit();
+    });
 
-	$('input#file_to_open').change(function() {
-		$('form#open_file_form').submit();
-	});
+    if (!actions.handleError()) {
 
-	$.ajax({
-		dataType : 'json',
-		url : 'rest?action=get-attributes',
-		context : document.body
-	}).done(function(data) {
-		console.log(data);
-		$.each(data, function(i, item) {
-			$('#data-table thead').append('<th>' + item.m_Name + '</th>');
-		});
-		$('#data-table').show();
-	}).error(function(err, a, b) {
-		showError("You have to open model or import any data file.");
-		console.log(err);
-	});
+	actions.loadHeaders();
+	actions.loadInstances();
 
-	$.ajax({
-		dataType : 'json',
-		url : 'rest?action=get-instances',
-		context : document.body
-	}).done(function(data) {
-		console.log(data);
-		$.each(data, function(i, item) {
-			console.log(item.m_AttValues);
-			var row = '<tr>';
-			for (var i = 0; i < item.m_AttValues.length; i++) {
-				row += '<td>' + item.m_AttValues[i] + '</td>';
-			}
-			row += '</tr>';
-			console.log(row);
-			$('#data-table tbody').append(row);
-		});
-		$('#data-table').show();
-	}).error(function(err, a, b) {
-		showError("You have to open model or import any data file.");
-		console.log(err);
-	});
+    }
 
 });
+
+function Actions() {
+};
+
+Actions.prototype.showError = function(message) {
+    $('div#error-box').show();
+    $('div#error-box').html(message);
+}
+
+Actions.prototype.showSuccess = function(message) {
+    $('div#success-box').show();
+    $('div#success-box').html(message);
+}
+
+Actions.prototype.getURLParameter = function(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+	var sParameterName = sURLVariables[i].split('=');
+	if (sParameterName[0] == sParam)
+	    return sParameterName[1];
+    }
+    return null;
+}
+
+Actions.prototype.handleError = function() {
+    if (this.getURLParameter('error') != null) {
+	if (this.getURLParameter('error') == 'import-error') {
+	    this.showError('This file format is not supported. Try to import correct CSV, XLS or ARFF file.');
+	}
+	return true;
+    } else {
+	return false;
+    }
+}
+
+Actions.prototype.loadHeaders = function() {
+
+    var actions = this;
+
+    $.ajax({
+	dataType : 'json',
+	url : 'rest?action=get-attributes',
+	context : document.body
+    }).done(function(data) {
+	$.each(data, function(i, item) {
+	    $('#data-table thead').append('<th title="' + item.m_Name + '"><div>' + item.m_Name + '</div></th>');
+	    $('#data-table tfoot').append('<th title="' + item.m_Name + '"><div>' + item.m_Name + '</div></th>');
+	});
+    }).error(function(err, a, b) {
+	actions.showError("You have to open model or import any data file.");
+	console.log(err);
+    });
+}
+
+Actions.prototype.loadInstances = function() {
+
+    var actions = this;
+
+    $('#data-table').hide();
+    $('#data-table thead').empty();
+    $('#data-table tbody').empty();
+    $('#data-table tfoot').empty();
+
+    $('div#table-container').css('overflow', 'hidden');
+    $('div#table-container').css('background-image', 'url(images/loading.gif)');
+    $('div#table-container').css('background-position', 'center center');
+    $('div#table-container').css('background-repeat', 'no-repeat');
+
+    var keyOf = function(array, v) {
+	for (var k = 0; k < array.length; k++) {
+	    if (array[k] == v) {
+		return k;
+	    }
+	}
+    }
+
+    $.ajax({
+	dataType : 'json',
+	url : 'rest?action=get-instances',
+	context : document.body
+    }).done(function(data) {
+	$.each(data, function(i, item) {
+	    console.log(item);
+	    var row = '<tr>';
+	    if (item.m_NumAttributes !== undefined) {
+		for (var i = 0; i < item.m_NumAttributes; i++) {
+		    if ($.inArray(i, item.m_Indices) > -1) {
+			row += '<td>' + item.m_AttValues[keyOf(item.m_Indices, i)] + '</td>';
+
+		    } else {
+			row += '<td>' + '-' + '</td>';
+		    }
+		}
+		row += '</tr>';
+		$('#data-table tbody').append(row);
+	    } else {
+		for (var i = 0; i < item.m_AttValues.length; i++) {
+
+		    row += '<td>' + item.m_AttValues[i] + '</td>';
+
+		}
+		row += '</tr>';
+		$('#data-table tbody').append(row);
+	    }
+	});
+	$('#data-table').show();
+	$('div#table-container').css('overflow', 'scroll');
+	$('div#table-container').css('background-image', 'none');
+	actions.showSuccess(data.length + " instances has been loaded.");
+    }).error(function(err, a, b) {
+	$('div#table-container').css('background-image', 'none');
+	actions.showError("You have to open model or import any data file.");
+	console.log(err);
+    });
+}
